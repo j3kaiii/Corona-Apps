d59112230ff247b3dfe048621b4d6b0cf964e44f
j3kaiii <j3kaiii@gmail.com>
2017-10-15 17:41:06 +0300

    asteroid

---
+++ main.lua
@@ -0,0 +1,10 @@
+local composer = require("composer")
+
+display.setStatusBar(display.HiddenStatusBar)
+
+math.randomseed(os.time())
+
+composer.gotoScene("menu")
+
+audio.reserveChannels(1)
+audio.setVolume(0.5, {channel = 1})
